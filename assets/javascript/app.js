1e6e5cf2d7cd21b13fa426fa7ae4f03271fe8acb
Ksenia Cruey <Joshua@crueyphotography.com>
2016-05-08 00:24:19 -0400

    fixed on click binding and implemented play pause functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
 		    a.addClass('gif'); // Added a class 
 		    a.attr('data-name', gifs[i]); // Added a data-attribute
-            a.attr('id', 'animalBtn');
+            a.addClass('animalBtn');
 		    a.text(gifs[i]); // Provided the initial button text
 		    $('#animalButtons').append(a); // Added the button to the HTML
 		} 
@@ -41,7 +41,7 @@ $(document).ready(function () {
 		return false;
 	})
 
-	$('button').on('click', function() {
+	$('#animalButtons').on('click', '.animalBtn', function() {
         var animal = $(this).data('name');
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -52,7 +52,7 @@ $(document).ready(function () {
             .done(function(response) {
                 // step 1: Run this file, click a button, and see what the data looks like in the browser's console. Open up the Object, then open up the data key, then open up 0. Study the keys and how the JSON is structured.
 
-                // console.log(response)
+                console.log(response)
 
                 // step 2: since the image information is inside of the data key then make a variable named results and set it equal to response.data
 
@@ -80,7 +80,11 @@ $(document).ready(function () {
                     //------------put step 3 in between these dashes--------------------
                     var animalDiv = $('<div>');
                     var p = $('<p>').text("Rating: " + results[i].rating);
-                    var animalImage = $('<img>').attr('src', results[i].images.fixed_height.url);
+                    var animalImage = $('<img>').attr('src', results[i].images.fixed_height_still.url);
+                    animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+                    animalImage.attr('data-animate', results[i].images.fixed_height.url);
+                    animalImage.attr('data-state', 'still');
+                    animalImage.addClass('animalGif');
 
                     animalDiv.append(p);
                     animalDiv.append(animalImage);
@@ -92,5 +96,38 @@ $(document).ready(function () {
                 }
 
             });
-    })
+    });
+      $('#animals').on('click', '.animalGif', function(){
+            //STEP ONE: study the html above. Look at all the data attributes. Run the file in the browser. Look at the images. After you fill in steps 1 and 2 you'll be able to pause gifs from giphy.
+
+            //STEP TWO: make a variable named state and then reference the button's data-state into it. Do not use .data('state'). It won't work the way we expect.
+
+            //---------------FILL IN CODE HERE FOR STEP TWO----------------------------
+            var state = $(this).attr('data-state');
+            var animate = $(this).attr('data-animate');
+            var still = $(this).attr('data-still');
+            console.log(animate);
+            //----------------------------------------------------
+
+            /*STEP THREE: 
+                * if variable state is equal to 'still' then 
+                    * update the src attribute of this image that you clicked on to what data-animate is equal to for this image
+                    * and update the data-state attribute to 'animate'
+                * if state does not equal 'still' then 
+                    * update the src attribute of this image that you clicked on to what data-still is equal to for this image
+                    * and update the data-state attribute to 'still'
+            */
+
+            //---------------FILL IN CODE HERE FOR STEP THREE----------------------------
+            if (state == 'still') {
+                $(this).attr('src', animate);
+                $(this).attr('data-state', 'animate');
+            } else {
+                $(this).attr('src', still);
+                $(this).attr('data-state', 'still');
+            }
+            //----------------------------------------------------
+
+            //STEP FOUR: open the file in the browser and click on the images. Then click again to pause.
+        });
 });
